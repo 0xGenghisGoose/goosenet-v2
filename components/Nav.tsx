5e33928a450f0aab1bd32fe2c320739c92a116ff
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-01-30 17:47:13 -0600

    nav bar base styling

---
+++ components/Nav.tsx
@@ -0,0 +1,13 @@
+export default function Nav() {
+	return (
+		<div className='border-2 border-blue-500 w-full h-1/3'>
+			<nav className='flex border-2 border-purple-400'>
+				<ul className='flex justify-left border-2 border-green-400 space-x-12 w-1/3 p-4 ml-auto'>
+					<li className=''>Projects</li>
+					<li>Price Feed</li>
+					<li>Articles</li>
+				</ul>
+			</nav>
+		</div>
+	);
+}
