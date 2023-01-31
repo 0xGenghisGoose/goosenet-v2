a6fb34350ae1b6002d5bd793aa32e689646f4d47
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-01-30 20:18:46 -0600

    more search styling

--- components/Content.tsx
+++ components/Content.tsx
@@ -2,10 +2,11 @@ export default function Content() {
 	return (
 		<>
 			<div className='flex border-2 border-yellow-400 justify-center mt-40 p-24 max-w-[75%] mx-auto'>
-				<p className='p-8 text-lg font-semibold tracking-wide w-[75%]'>
-					Welcome to Goosenet, a collection of short write ups on multiple
-					random fucking protocols & tokens as well as some schizo musings from
-					a degen just tryna keep their head above water
+				<p className='p-8 text-xl font-semibold tracking-wider w-[75%]'>
+					welcome to <span className='text-red-500'>goosenet</span>, a
+					collection of short write ups on multiple random fucking protocols &
+					tokens as well as some schizo musings from a degen just tryna keep
+					their head above water
 				</p>
 			</div>
 		</>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,7 +6,7 @@ export default function SearchBar() {
 			<div className='flex relative items-center rounded-md border border-gray-400 font-medium'>
 				<MagnifyingGlassIcon className='h-4 absolute pl-1 text-black' />
 				<input
-					className='border border-pink-900 min-w-[75%] pl-6 text-md'
+					className='min-w-[75%] pl-6 text-md text-black focus:outline-red-500'
 					type='text'
 					placeholder='Search'></input>
 			</div>
