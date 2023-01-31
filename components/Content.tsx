dd2c30a6858e209a87f3e0268fa31ceb6e13afb8
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-01-30 19:54:36 -0600

    getting gradients done

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,11 +1,11 @@
 export default function Content() {
 	return (
 		<>
-			<div className='flex border-2 border-yellow-400 justify-center mb-0 p-8'>
-				<p className=''>
-					Content going in here baby, lots of it, make sure you have enough
-					room, etc. Gonna be a lil paragraph so I guess make sure you have
-					enough room for that
+			<div className='flex border-2 border-yellow-400 justify-center mt-40 p-24 max-w-[75%] mx-auto'>
+				<p className='p-8 text-lg font-semibold tracking-wide w-[75%]'>
+					Welcome to Goosenet, a collection of short write ups on multiple
+					random fucking protocols & tokens as well as some schizo musings from
+					a degen just tryna keep their head above water
 				</p>
 			</div>
 		</>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ import SearchBar from './SearchBar';
 
 export default function Nav() {
 	return (
-		<div className='border-2 border-blue-500 w-full h-2/3 p-4'>
+		<div className='border-2 border-blue-500 w-full h-2/3 p-4 bg-gradient-to-r from-gray-600 via-slate-500 to-slate-600'>
 			<nav className='flex border-2 border-purple-400'>
 				goosenet logo
 				<ul className='flex justify-left items-center border-2 border-green-400 space-x-16 min-w-1/3 p-4 ml-auto'>
