e27f093129ac5f39784fb48992eb09a2c565734e
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-01-30 20:08:07 -0600

    logo imported and rendering correctly

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,14 @@
 import SearchBar from './SearchBar';
 
 // To Do:
-// Add logo
-// Add color
+// Add route nav
 // Sticky?
 
 export default function Nav() {
 	return (
-		<div className='border-2 border-blue-500 w-full h-2/3 p-4 bg-gradient-to-r from-gray-600 via-slate-500 to-slate-600'>
-			<nav className='flex border-2 border-purple-400'>
-				goosenet logo
+		<div className='border-2 border-blue-500 w-full h-2/3 p-4 bg-gradient-to-r from-gray-700 via-slate-600 to-slate-700'>
+			<nav className='flex items-center border-2 border-purple-400'>
+				<img src='/goosenet-01.png' className='h-10'></img>
 				<ul className='flex justify-left items-center border-2 border-green-400 space-x-16 min-w-1/3 p-4 ml-auto'>
 					<li className=''>Projects</li>
 					<li>Price Feed</li>
