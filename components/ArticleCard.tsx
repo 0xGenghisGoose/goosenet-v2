a5e76e88f8b4b66df83d8fd5b29c2d154e330f18
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-08 15:21:53 -0600

    dates parsing correctly

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,12 +1,17 @@
+import dayjs from 'dayjs';
+
 // To Do: type article response & get rid of any
 // Add styling to card - hover effects, etc
+// Figure out why timestamp from db is pulling weird
 
 export default function ArticleCard({ articles }: any) {
 	return (
 		<>
-			<div className='p-8 m-4 hover:bg-slate-700 border-b border-dotted'>
+			<div className='p-8 m-4 hover:bg-slate-700 border-b border-dotted border-slate-500/80'>
 				<h1 className='text-xl text-red-500 p-1'>{articles.data().title}</h1>
-				<h2 className='p-1'>{articles.data().author}</h2>
+				<h2 className='p-2'>
+					{dayjs.unix(articles.data().timestamp).format('MMMM D, 2023')}
+				</h2>
 				<p className='truncate pt-2'>{articles.data().content}</p>
 				<button className='p-2 px-5 mt-5 rounded-xl bg-red-500 hover:bg-red-400 shadow-md shadow-red-500/40'>
 					Read
