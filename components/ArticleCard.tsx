3961f9d41676288663a4e932cb85a5efcf81eae4
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-08 14:51:12 -0600

    styling article render list

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,12 +1,16 @@
 // To Do: type article response & get rid of any
+// Add styling to card - hover effects, etc
 
 export default function ArticleCard({ articles }: any) {
 	return (
 		<>
-			<div className='p-8 m-4 border-2 border-purple-500'>
-				<h1 className='text-xl p-1'>{articles.data().title}</h1>
+			<div className='p-8 m-4 hover:bg-slate-700 border-b border-dotted'>
+				<h1 className='text-xl text-red-500 p-1'>{articles.data().title}</h1>
 				<h2 className='p-1'>{articles.data().author}</h2>
 				<p className='truncate pt-2'>{articles.data().content}</p>
+				<button className='p-2 px-5 mt-5 rounded-xl bg-red-500 hover:bg-red-400 shadow-md shadow-red-500/40'>
+					Read
+				</button>
 			</div>
 		</>
 	);
