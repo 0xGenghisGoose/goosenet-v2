4507788e20ebabcd15a6168d6e991514a1de438c
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-14 10:53:11 -0600

    to dos, scaling banner img, set timeout

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,5 +1,7 @@
 import dayjs from 'dayjs';
 
+// To Do: Figure out how to scale banner img down OR put in bg
+
 export default function Article({ article }: any) {
 	return (
 		<div className='border-2 border-pink-400 text-center'>
@@ -11,16 +13,16 @@ export default function Article({ article }: any) {
 					className='object-fit h-full w-full'></img>
 			</div>
 			<div className='p-10 m-4'>
-				{/* Title, time to read, author, timestamp, etc */}
-				<div className='pb-4 font-bold tracking-wider text-3xl'>
+				{/* Title, time to read, timestamp */}
+				<div className='pb-4 font-extrabold tracking-widest text-3xl text-red-500'>
 					{article?.title}
 				</div>
-				<div className='pb-2 text-red-500'>{article?.author}</div>
-				<div className='pb-8'>
+
+				<div className='pb-4 mx-96 border-b border-dotted border-slate-500/60 tracking-wide'>
 					{dayjs.unix(article?.timestamp).format('MMMM D, 2023')}
 				</div>
 				{/* Article copy */}
-				<p className='p-2 pb-8 m-4'>{article?.content}</p>
+				<p className='p-4 pb-8 m-4'>{article?.content}</p>
 			</div>
 		</div>
 	);
