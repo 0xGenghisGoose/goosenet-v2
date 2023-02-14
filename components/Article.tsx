0e5c80f2253843906ae299ead658bde5714a1d1d
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-14 10:46:34 -0600

    article page styling

--- components/Article.tsx
+++ components/Article.tsx
@@ -4,22 +4,23 @@ export default function Article({ article }: any) {
 	return (
 		<div className='border-2 border-pink-400 text-center'>
 			{/* Banner Image */}
-			<div className='border-2 border-yellow-300'>
+			<div className='h-2/3'>
 				<img
 					src={article?.bannerImg}
 					alt='banner-img'
-					className=''
-					height='20vh'></img>
+					className='object-fit h-full w-full'></img>
 			</div>
-			<div className='p-12'>
+			<div className='p-10 m-4'>
 				{/* Title, time to read, author, timestamp, etc */}
-				<div className='pb-4'>{article?.title}</div>
+				<div className='pb-4 font-bold tracking-wider text-3xl'>
+					{article?.title}
+				</div>
 				<div className='pb-2 text-red-500'>{article?.author}</div>
 				<div className='pb-8'>
 					{dayjs.unix(article?.timestamp).format('MMMM D, 2023')}
 				</div>
 				{/* Article copy */}
-				<p className='p-2 m-4'>{article?.content}</p>
+				<p className='p-2 pb-8 m-4'>{article?.content}</p>
 			</div>
 		</div>
 	);
