58aaf6167a5e9c9e0bee637efe9f216fdd82c39d
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-22 14:46:29 -0600

    content responsive for small screens

--- components/Content.tsx
+++ components/Content.tsx
@@ -4,8 +4,8 @@
 export default function Content() {
 	return (
 		<>
-			<div className='flex shadow-sm justify-center mt-40 py-24 px-36 max-w-[75%] mx-auto'>
-				<p className='p-8 text-xl font-semibold tracking-wider leading-10 w-[65%]'>
+			<div className='flex shadow-sm justify-center mt-40 py-24 sm:py-14 sm:px-16 sm:mt-8 px-36 max-w-[75%] mx-auto'>
+				<p className='p-8 text-xl font-semibold tracking-wider leading-10 w-[65%] sm:w-[80%]'>
 					welcome to <span className='text-red-500'>goosenet</span>, a
 					collection of short write ups on multiple random fucking protocols &
 					tokens as well as some schizo musings from a degen just tryna keep
