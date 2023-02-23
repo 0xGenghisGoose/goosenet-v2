24403ca65a87e06d9c49103468b0c37e0a3c10d1
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-23 17:44:44 -0600

    responsive homepage & nav

--- components/Content.tsx
+++ components/Content.tsx
@@ -4,7 +4,7 @@
 export default function Content() {
 	return (
 		<>
-			<div className='flex shadow-sm justify-center mt-2 md:mt-36 py-6 md:py-24 px-4 md:px-36 max-w-[75%] mx-auto'>
+			<div className='flex shadow-sm justify-center mt-20 md:mt-32 lg:mt-36 py-6 lg:py-24 px-4 lg:px-36 max-w-[75%] mx-auto border border-pink-400'>
 				<p className='p-8 text-lg md:text-xl font-semibold tracking-wide md:tracking-wider leading-10 w-[85%] md:w-[65%]'>
 					welcome to <span className='text-red-500'>goosenet</span>, a
 					collection of short write ups on multiple random fucking protocols &
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 import SearchBar from './SearchBar';
 
-// Need md && lg breakpoints
-
 export default function Nav() {
 	return (
 		<div className='sticky top-0 w-full p-2 md:p-4 bg-gradient-to-r from-gray-700 via-slate-600 to-slate-700 border-b border-red-400/70'>
@@ -11,10 +9,10 @@ export default function Nav() {
 					<img
 						src='/goosenet-01.png'
 						alt='goosenet-logo'
-						className='h-8 md:h-10 shrink-0'></img>
+						className='h-8 lg:h-10 shrink-0'></img>
 				</Link>
 
-				<ul className='flex justify-left items-center md:space-x-16 space-x-8 p-4 ml-auto'>
+				<ul className='flex justify-left items-center text-sm md:text-lg lg:space-x-16 space-x-8 p-4 ml-auto'>
 					<li className='hover:text-red-500'>Projects</li>
 					<li className='hover:text-red-500'>
 						<Link href='/PriceFeed' className='hover:text-red-500'>
