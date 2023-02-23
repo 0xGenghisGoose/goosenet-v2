166785783be55fbef4f79e7ef49f77ac70b97fca
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-23 17:53:43 -0600

    price feed responsive

--- pages/PriceFeed.tsx
+++ pages/PriceFeed.tsx
@@ -5,15 +5,13 @@ import FeedCard from '@/components/FeedCard';
 import Footer from '@/components/Footer';
 import { fullCoinAPI, fetcher } from '@/utils';
 
-// API call to CoinGecko - maps coins out & passes data to individual component - when token selected, routed to own personal page w/ dynamic routing
-
 export default function PriceFeed() {
 	const { data } = useSWR(fullCoinAPI, fetcher);
 
 	return (
 		<>
 			<Head>
-				<title>goosenet</title>
+				<title>goosenet - Price Feed</title>
 				<meta name='description' content='Goosenet by 0xGenghisGoose' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
@@ -23,17 +21,16 @@ export default function PriceFeed() {
 				{/* Nav */}
 				<Nav />
 
-				<h2 className='text-center text-4xl text-red-500 font-bold tracking-widest pt-4'>
+				<h2 className='text-center text-3xl md:text-4xl text-red-500 font-bold tracking-widest pt-4'>
 					PRICE FEED
 				</h2>
 				<br />
-				<h4 className='text-center text-xl font-semibold tracking-wider pb-4 m-6 mx-96 border-b border-dotted border-slate-500/60'>
+				<h4 className='text-center text-md lg:text-xl font-semibold tracking-wider pb-4 m-6 mx-72 lg:mx-96 border-b border-dotted border-slate-500/60'>
 					Top <span className='text-red-500'>100</span> Tokens By Market Cap
 				</h4>
 
-				{/* To Do: add onclick to be taken to specific asset page */}
 				<div className='justify-center'>
-					<div className='p-2 pb-8 m-4 text-center text-lg min-w-screen'>
+					<div className='p-2 pb-8 m-4 text-center text-md lg:text-lg min-w-screen'>
 						{data?.map((token: any) => (
 							<FeedCard key={token.id} token={token}></FeedCard>
 						))}
