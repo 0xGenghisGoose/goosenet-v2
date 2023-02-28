57ce56671ca4da9849a0465f646d8c966658e854
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-28 16:27:44 -0600

    closer, will use agoria

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,19 +1,33 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { getDocs, collection, DocumentData } from 'firebase/firestore';
+import { db } from '@/firebase';
 
+// Might need to make state from db shareable across whole app
 export default function SearchBar() {
 	const [searchInput, setSearchInput] = useState('');
+	const [enterSearch, setEnterSearch] = useState('');
+	const [articles, setArticles] = useState<DocumentData[]>([]);
 	const router = useRouter();
 
 	const handleChange = (e: any) => {
 		e.preventDefault();
-		setSearchInput(e.target.value);
 	};
 
-	if (searchInput.length > 0) {
-		router.push(`/articles/${searchInput}`);
-	}
+	const handleKeyDown = (e: any) => {
+		e.preventDefault();
+		if (e.key === 'Enter') {
+			setSearchInput(e.target.value);
+			try {
+				router.push(`/articles/${searchInput}`);
+			} catch (err) {
+				console.error(err);
+			}
+		}
+	};
+
+	const compare = () => {};
 
 	return (
 		<>
@@ -23,8 +37,9 @@ export default function SearchBar() {
 					className='max-w-[75%] md:min-w-[85%] pr-0 pl-6 text-sm md:text-md text-black focus:outline-red-500'
 					type='text'
 					placeholder='Search'
+					value={searchInput}
 					onChange={handleChange}
-					value={searchInput}></input>
+					onKeyDown={handleKeyDown}></input>
 			</div>
 		</>
 	);
