fd6ec73c1c9fc181602544b69fe22cc2f6dac82c
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-02-28 15:47:15 -0600

    first stab at search input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,20 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
-// To Do: incorporate search functionality
 export default function SearchBar() {
+	const [searchInput, setSearchInput] = useState('');
+	const router = useRouter();
+
+	const handleChange = (e: any) => {
+		e.preventDefault();
+		setSearchInput(e.target.value);
+	};
+
+	if (searchInput.length > 0) {
+		router.push(`/articles/${searchInput}`);
+	}
+
 	return (
 		<>
 			<div className='flex relative items-center rounded-lg font-medium'>
@@ -9,7 +22,9 @@ export default function SearchBar() {
 				<input
 					className='max-w-[75%] md:min-w-[85%] pr-0 pl-6 text-sm md:text-md text-black focus:outline-red-500'
 					type='text'
-					placeholder='Search'></input>
+					placeholder='Search'
+					onChange={handleChange}
+					value={searchInput}></input>
 			</div>
 		</>
 	);
