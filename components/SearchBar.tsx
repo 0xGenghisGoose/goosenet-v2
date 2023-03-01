0f2b7c6636300d4273f4d36532d6f6835d2a3a64
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2023-03-01 15:23:11 -0600

    remove unworking search code

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,33 +1,10 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { getDocs, collection, DocumentData } from 'firebase/firestore';
-import { db } from '@/firebase';
 
 // Might need to make state from db shareable across whole app
 export default function SearchBar() {
 	const [searchInput, setSearchInput] = useState('');
 	const [enterSearch, setEnterSearch] = useState('');
-	const [articles, setArticles] = useState<DocumentData[]>([]);
-	const router = useRouter();
-
-	const handleChange = (e: any) => {
-		e.preventDefault();
-	};
-
-	const handleKeyDown = (e: any) => {
-		e.preventDefault();
-		if (e.key === 'Enter') {
-			setSearchInput(e.target.value);
-			try {
-				router.push(`/articles/${searchInput}`);
-			} catch (err) {
-				console.error(err);
-			}
-		}
-	};
-
-	const compare = () => {};
 
 	return (
 		<>
@@ -37,9 +14,7 @@ export default function SearchBar() {
 					className='max-w-[75%] md:min-w-[85%] pr-0 pl-6 text-sm md:text-md text-black focus:outline-red-500'
 					type='text'
 					placeholder='Search'
-					value={searchInput}
-					onChange={handleChange}
-					onKeyDown={handleKeyDown}></input>
+					value={searchInput}></input>
 			</div>
 		</>
 	);
